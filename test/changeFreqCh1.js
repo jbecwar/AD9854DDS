a69a1b0985e570b3c719c558f3a0ff1bd4335e4c
James Becwar <james.becwar@knomatic.com>
2015-10-18 01:08:29 -0500

    command strings are working, but ecc code is off.  But after reviewing the micro code I don't think its even used. (the etc)

--- test/changeFreqCh1.js
+++ test/changeFreqCh1.js
@@ -1,9 +1,50 @@
 var Code = require('code'); // assertion library
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
+var lib = require('../lib/ad9854.js');
 
-lab.test('returns true when 1 + 1 equals 2', function(done) {
+lab.test('Tests changing freq on Ch1', function(done) {
 
-  Code.expect(1 + 1).to.equal(2);
+  lib.setMultiplier(10);
+
+  var result = lib.setFreq(1);
+  Code.expect(result).to.equal("0240A70D74DA0045");
+
+  result = lib.setFreq(10);
+  Code.expect(result).to.equal("02888888888808B3");
+
+  result = lib.setFreq(100);
+  Code.expect(result).to.equal("0255555555555501");
+
+  //Not sure if I copyed down the wrong value... calculates 0x07 for ecc
+  result = lib.setFreq(0.1);
+  Code.expect(result).to.equal("02ECC367D8150006");
+
+  lib.setMultiplier(1);
+
+  result = lib.setFreq(1);
+  Code.expect(result).to.equal("02888888888808B3");
+
+  result = lib.setFreq(10);
+  Code.expect(result).to.equal("0255555555555501");
+
+  // result = lib.setFreq(100);
+  // Code.expect(result).to.equal("crash in example...");
+
+  lib.setMultiplier(5);
+
+  result = lib.setFreq(1);
+  Code.expect(result).to.equal("02814E1BE8B4018A");
+
+  //this fails  it calculates 67
+  result = lib.setFreq(10);
+  Code.expect(result).to.equal("0211111111111169");
+
+  //this fails  result is 01 for the ecc byte
+  result = lib.setFreq(100);
+  Code.expect(result).to.equal("02AAAAAAAAAAAAFF");
+
+
+
   done();
 });
